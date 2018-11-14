acfc513fbec3fab39874099e6e66d32646e66959
Gerardo Arjona <gerardoarjona@Gerardos-MacBook-Pro.local>
2018-11-14 10:33:10 -0600

    Add Primera Clase: Repaso
    Signed-off-by: Gerardo Arjona <gerardoarjona@Gerardos-MacBook-Pro.local>

---
+++ repaso_clase1/main.js
@@ -0,0 +1,66 @@
+//alert("Estoy conectado con el HTML")
+
+
+//Variables
+// var variableNumero = 2;
+// var variableTexto = "Este es un texto";
+// var variableBooleana = true;
+
+// console.log("Texto de prueba a consola");
+// console.log(variableNumero);
+// console.log(variableTexto);
+// console.log(variableBooleana);
+
+// console.log(typeof(variableBooleana));
+
+//let miEdad = 5;
+//const pie = 3.1416;
+// //Concatenacion
+// console.log("Mi edad es: "+ pi);
+
+// if(miEdad >= 18 || miEdad <=6){
+//     console.log("Bienvenido al bar!");
+// }else if(miEdad > 60){
+//     console.log("Ya estas muy viejo, que haces aqui?");
+// }else{
+//     console.log("Eres menor de edad")
+// }
+ //For
+
+//  for(let i=0; i<11; i++){
+//     console.log(i);
+//  }
+
+//  for(let i=0; i<=25; i++){
+//      if(i%2 == 0){
+//          console.log(i)
+//      }
+//  }
+
+// A partir de una variable que contenga una cadena de Texto.
+// Se debe mostrar mediante un console.log esa misma cadena pero
+// alternando entre minusculas y mayusculas 
+
+// let = "perritosalchicha"
+
+// El resultado en consola debe mostrarse como:
+//  "pErRiToSaLcHiChA"
+
+// if(2 === "2"){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// let n1 = 2;
+// let n2 = 2;
+
+// function SumaDosNumeros(num1, num2){
+//     console.log(num1);
+//     console.log(num2);
+//     suma = num1 + num2;
+//     return suma
+// }
+
+// suma = SumaDosNumeros(n2,n1);
+// console.log(suma)
